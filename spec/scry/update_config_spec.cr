6b1b142b1132bc92bf65f9e9363d0e0c77c3d56b
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-25 12:53:18 -0500

    Change  io.json_object by JSON.buils

--- spec/scry/update_config_spec.cr
+++ spec/scry/update_config_spec.cr
@@ -3,36 +3,32 @@ require "json"
 require "../../src/scry/update_config"
 
 module Scry
-
   describe UpdateConfig do
-
     it "updates the maximum number of problems to report" do
       workspace = Workspace.new("/foo", 1.to_i64, 10)
       updater = UpdateConfig.new(
         workspace,
         DidChangeConfigurationParams.from_json(
-          String.build { |io|
-            io.json_object do |params|
+          JSON.build do |params|
+            params.object do
               params.field "settings" do
-                io.json_object do |settings|
-                  settings.field "crystal-ide" do
-                    io.json_object do |crystal_ide|
-                      crystal_ide.field "maxNumberOfProblems", 20
-                      crystal_ide.field "backend", "scry"
-                      crystal_ide.field "customCommand", "crystal"
-                      crystal_ide.field "customCommandArgs", [] of String
-                      crystal_ide.field "logLevel", "debug"
+                params.object do
+                  params.field "crystal-lang" do
+                    params.object do
+                      params.field "backend", "scry"
+                      params.field "customCommand", "crystal"
+                      params.field "customCommandArgs", [] of String
+                      params.field "logLevel", "debug"
                     end
                   end
                 end
               end
             end
-          }
+          end
         )
       )
       changed, _response = updater.run
       changed.max_number_of_problems.should eq(20)
     end
   end
-
 end
