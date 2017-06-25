9e7bbe539271b91d95f319e21559ca361ed3ba22
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-25 12:39:26 -0500

    Fix json examples

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -9,7 +9,7 @@ module Scry
   SOME_FILE_PATH = File.expand_path("./fixtures/some_file.cr", __DIR__)
 
   INITIALIZATION_EXAMPLE =
-    %({ "jsonrpc": "2.0", "id": 1, "method": "initialize", "params": { "processId": 1, "rootPath": "/foo", "capabilities": {} }})
+    %({ "jsonrpc": "2.0", "id": 1, "method": "initialize", "params": { "processId": 1, "rootPath": "/foo", "capabilities": {} , "trace": "off"}})
 
   CONFIG_CHANGE_EXAMPLE =
     %({"jsonrpc":"2.0","method":"workspace/didChangeConfiguration","params":{"settings":{"crystal-ide":{"maxNumberOfProblems":100,"backend":"scry","customCommand":"crystal","customCommandArgs":[],"logLevel":"debug"}}}})
@@ -18,7 +18,7 @@ module Scry
     %({"jsonrpc":"2.0","method":"textDocument/didOpen","params":{"textDocument":{"uri":"file:///Users/foo/Projects/crystal_av/src/crystal_av/c_handler.cr","languageId":"crystal","version":1,"text":"put \\"hello\\"; Thing.new"}}})
 
   DOC_CHANGE_EXAMPLE =
-    %({"jsonrpc":"2.0","method":"textDocument/didChange","params":{"textDocument":{"uri":"file://#{SOME_FILE_PATH}","version":808},"contentChanges":[{"text":"module Scry\n\n  put this\n"}]}})
+    %({"jsonrpc":"2.0","method":"textDocument/didChange","params":{"textDocument":{"version":808,"uri":"file://#{SOME_FILE_PATH}"},"contentChanges":[{"text":"module Scry\\n\\n  put this\\n"}]}})
 
   WATCHED_FILE_CHANGED_EXAMPLE =
     %({"jsonrpc":"2.0","method":"workspace/didChangeWatchedFiles","params":{"changes":[{"uri":"file://#{SOME_FILE_PATH}","type":2}]}})
