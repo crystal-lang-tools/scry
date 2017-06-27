b9fa5c92a1c7aecb67a7bc098a9401d44f97e3b6
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-27 11:19:52 -0500

    Add support for vscode-crystal-lang

--- src/scry/protocol/ide_customizations.cr
+++ src/scry/protocol/ide_customizations.cr
@@ -4,12 +4,12 @@ module Scry
 
   struct IDECustomizations
     JSON.mapping({
-      max_number_of_problems: { type: Int32, key: "maxNumberOfProblems" },
-      backend: String,
-      custom_command: { type: String, key: "customCommand" },
-      custom_command_args: { type: Array(String), key: "customCommandArgs" },
-      log_level: { type: String, key: "logLevel" }
-    }, true)
+      max_number_of_problems: { type: Int32, key: "problemsLimit" },
+      backend: { type: String, default: "scry" },
+      custom_command: { type: String, key: "customCommand", default: "" },
+      custom_command_args: { type: Array(String), key: "customCommandArgs", default: Array(String).new },
+      log_level: { type: String, key: "logLevel", default: "error" }
+    })
   end
 
 end
--- src/scry/protocol/settings.cr
+++ src/scry/protocol/settings.cr
@@ -4,7 +4,7 @@ require "./ide_customizations"
 module Scry
   struct Settings
     JSON.mapping({
-      crystal_ide: { type: IDECustomizations, key: "crystal-ide" }
+      crystal_ide: { type: IDECustomizations, key: "crystal-lang" }
     }, true)
   end
 end
