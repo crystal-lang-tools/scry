40436f0e394970d888e0269b7bd3402241ac59fc
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-07-19 11:15:24 -0500

    Check if uri starts with git://

--- src/scry/text_document.cr
+++ src/scry/text_document.cr
@@ -14,32 +14,32 @@ module Scry
     getter position : Position?
 
     def initialize(@uri, @text)
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
     end
 
     def initialize(params : DidOpenTextDocumentParams)
       @uri = params.text_document.uri
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
       @text = [params.text_document.text]
     end
 
     # Used by ParseAnalyzer
     def initialize(change : DidChangeTextDocumentParams)
       @uri = change.text_document.uri
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
       @text = change.content_changes.map { |change| change.text }
     end
 
     # Used by Analyzer
     def initialize(file_event : FileEvent)
       @uri = file_event.uri
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
       @text = [read_file]
     end
 
     def initialize(params : DocumentFormattingParams, @id)
       @uri = params.text_document.uri
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
       if untitled?
         @text = [""]
       else
@@ -50,12 +50,16 @@ module Scry
     def initialize(params : TextDocumentPositionParams, @id)
       @uri = params.text_document.uri
       @position = params.position
-      @filename = @uri.sub(/^file:\/\/|^inmemory:\/\//, "")
+      @filename = uri_to_filename
       @text = [read_file]
     end
 
+    def uri_to_filename
+      @uri.sub(/^file:\/\/|^inmemory:\/\/|^git:\/\//, "")
+    end
+
     def in_memory?
-      uri.starts_with?("inmemory://")
+      uri.starts_with?("inmemory://") || uri.starts_with?("git://")
     end
 
     def untitled?
