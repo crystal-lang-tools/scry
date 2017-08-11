909565ddd251bebf86f78e2bc546eb508edeca8b
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-08-10 18:57:33 -0600

    Allow trace to be nilable.
    This ensures compatibility with Atom and is more accurate to LSP

--- src/scry/protocol/initialize_params.cr
+++ src/scry/protocol/initialize_params.cr
@@ -6,7 +6,7 @@ module Scry
       process_id:   {type: Int64 | Int32, key: "processId"},
       root_uri:     {type: String, key: "rootPath"},
       capabilities: JSON::Any,
-      trace:        String,
+      trace:        String?,
     })
 
   end
