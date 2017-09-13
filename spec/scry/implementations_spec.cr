d1263be962bd2958c0f3950b8af367759c470b27
Faustino Aguilar <faustinoaq@gmail.com>
2017-09-13 08:09:30 -0500

    Update and optimizations.
    - Update Dockerfile.
    - Remove release mode.
    - Clean specs.

--- spec/scry/implementations_spec.cr
+++ spec/scry/implementations_spec.cr
@@ -8,13 +8,5 @@ module Scry
       impl = Implementations.new(text_document)
       impl.run.is_a?(ResponseMessage).should eq(true)
     end
-
-    # it "check implementation response content on Travis CI" do
-    #   params = TextDocumentPositionParams.from_json(TEXTDOCUMENT_POSITION_PARAM_EXAMPLE)
-    #   text_document = TextDocument.new(params, 0)
-    #   impl = Implementations.new(text_document)
-    #   response = impl.run
-    #   response.to_json.should eq(IMPLEMENTATIONS_RESPONSE_EXAMPLE)
-    # end
   end
 end
