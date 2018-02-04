d846776211e753fed76cadfb9b8d0803be3fd5af
Filipe Correia <filipe.lp.correia@gmail.com>
2018-02-04 20:29:45 +0000

    Refactors some use cases

--- spec/scry/text_document_spec.cr
+++ spec/scry/text_document_spec.cr
@@ -6,15 +6,5 @@ module Scry
       text_document = TextDocument.new("inmemory://model/3", [%(puts "foo")])
       text_document.in_memory?.should be_true
     end
-
-    it "handles updates to content" do
-      path = "spec/fixtures/completion/sample.cr"
-      uri = "file://#{File.expand_path(path)}"
-      text_document = TextDocument.new(uri, [File.read(path)])
-      text_document.update_change(TextDocumentContentChangeEvent.from_json(%({"range": {"start": {"line": 2, "character": 5}, "end": {"line": 2, "character": 15}}, "text": "def method\\n a + b\\n"} )))
-      text_document.text.should eq([
-        "class A\n    def method\n a + b\n    end\nend"
-      ])
-    end
   end
 end
--- src/scry/completion.cr
+++ src/scry/completion.cr
@@ -1,15 +1,26 @@
 require "./log"
+require "compiler/crystal/crystal_path"
 
 module Scry
   # struct CompletionContext
   #   type
   # end
-  alias AutoCompletionContext = InstanceMethodCallContext
+
+  abstract class AutoCompletionContext
+    abstract def find : Array(CompletionItem)
+  end
   class UnrecognizedContext < Exception
   end
 
-  struct InstanceMethodCallContext
-    def initialize(@target : String, @method : String)
+  class InstanceMethodCallContext < AutoCompletionContext
+    def initialize(@target : String, @method : String, @line : String, @text_document : TextDocument)
+    end
+
+    def find
+      # to_completion_items(["Results", "And", "more", "results"])
+      # TODO implement method call comlpetion
+
+      to_completion_items([Crystal::CrystalPath.default_path])
     end
 
     def to_completion_items(results : Array(String))
@@ -19,25 +30,42 @@ module Scry
     end
   end
 
+  class InstanceVariableContext < AutoCompletionContext
+    def initialize(@var : String, @line : String, @text_document : TextDocument)
+    end
+
+    def find
+      # to_completion_items(["Results", "And", "more", "results"])
+      # TODO implement instance variable completion
+      [] of CompletionItem
+    end
+
+    def to_completion_items(results : Array(String))
+      results.map do |res|
+        CompletionItem.new(res, CompletionItemKind::Variable, res)
+      end
+    end
+  end
+
+
   class Completion
     METHOD_CALL_REGEX = /(?<target>[a-zA-Z][a-zA-Z_:]*)\.(?<method>[a-zA-Z]*[a-zA-Z_:]*)$/
+    INSTANCE_VARIABLE_REGEX = /(?<var>@[a-zA-Z_]*)$/
     def initialize(@text_document : TextDocument, @context : CompletionContext | Nil, @position : Position)
     end
 
     def run
       context = parse_context
-      find_with_context(context)
-    end
-
-    def find_with_context(context : InstanceMethodCallContext)
-      context.to_completion_items(["Results", "And", "more", "results"])
+      context.find
     end
 
     def parse_context
       line = @text_document.text.first.lines[@position.line][0..@position.character]
-
-      if(match = line.match(METHOD_CALL_REGEX))
-        InstanceMethodCallContext.new(match["target"], match["method"])
+      case line
+      when METHOD_CALL_REGEX
+        InstanceMethodCallContext.new($~["target"], $~["method"], line , @text_document)
+      when INSTANCE_VARIABLE_REGEX
+        InstanceVariableContext.new($~["var"], line, @text_document)
       else
         raise UnrecognizedContext.new("Couldn't identify context of: #{line}")
       end
