97b44dcae534c5b904c44d4f1a3ddf5386b6d4ea
Faustino Aguilar <faustinoaq@gmail.com>
2018-03-03 22:30:47 -0500

    Trying to implement a concurrent rpc for scry

--- src/scry.cr
+++ src/scry.cr
@@ -4,48 +4,13 @@ require "./scry/context"
 require "./scry/message"
 require "./scry/response"
 require "./scry/environment_config"
+require "./scry/concurrent_rpc"
 
 module Scry
   def self.start
     Log.logger.info("Scry is looking into your code...")
-
-    at_exit do
-      Log.logger.info("...your session has ended")
-    end
-
     EnvironmentConfig.new.run
-
-    next_request = Channel(Int32).new(10)
-    context = Context.new
-
-    loop do |i|
-      spawn do
-        Log.logger.debug("Scry is listening request ##{i}...")
-        content = Request.new(STDIN).read
-        request = Message.new(content).parse
-        results = context.dispatch(request)
-      rescue ex
-        results = [ResponseMessage.new(ex)]
-      ensure
-        response = Response.new([results].flatten)
-        response.write(STDOUT)
-        next_request.send i
-      end
-
-      select
-      when n = next_request.receive
-        Log.logger.debug("Scry has processed request ##{n}!")
-        next
-      else
-        sleep 1
-      end
-    end
-  rescue ex
-    Log.logger.error(
-      %(#{ex.message || "Unknown error"}\n#{ex.backtrace.join('\n')})
-    ) unless Log.logger.nil?
-  ensure
-    Log.logger.close unless Log.logger.nil?
+    ConcurrentRpc.new.run
   end
 end
 
