2e988b759fac626cb57349d41cf8e2fba5c3dd1d
Filipe Correia <laginha87@users.noreply.github.com>
2018-03-09 14:01:38 +0000

    On ci check that code is formatted (#57)

--- src/scry/completion_resolver.cr
+++ src/scry/completion_resolver.cr
@@ -14,8 +14,8 @@ module Scry
       when MethodCallContextData
         file = File.new data.path
         line = data.location.not_nil!.split(":")[1].to_i
-        lines = file.each_line.first(line-1).to_a.reverse.take_while(&.match /^\s*#/).map{|e| e.gsub(/^\s*#/, "") }.reverse.join("\n")
-        @completionItem.documentation =  MarkupContent.new("markdown",  lines)
+        lines = file.each_line.first(line - 1).to_a.reverse.take_while(&.match /^\s*#/).map { |e| e.gsub(/^\s*#/, "") }.reverse.join("\n")
+        @completionItem.documentation = MarkupContent.new("markdown", lines)
         @completionItem
       else
         @completionItem
