c0788776be77a86dfea6e006a31a83e960d22d25
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-09 11:23:10 -0500

    Makes processId nilable

--- src/scry/protocol/initialize_params.cr
+++ src/scry/protocol/initialize_params.cr
@@ -3,7 +3,7 @@ require "json"
 module Scry
   struct InitializeParams
     JSON.mapping({
-      process_id:   {type: Int64 | Int32 | Nil, key: "processId"},
+      process_id:   {type: Int64 | Int32 | Nil, key: "processId", nilable: true},
       root_uri:     {type: String | Nil, key: "rootUri"},
       root_path:    {type: String | Nil, key: "rootPath", nilable: true},
       capabilities: JSON::Any,
