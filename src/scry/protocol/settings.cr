4aae9aa4ed5d11d6b9933d39dff220818d3f0713
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-09 08:06:29 -0500

    Makes scry settings editor agnostic
    * Remove vscode (crystal-ide, crystal-lang) and atom specific settings
    * Read configuration from any settings starting with crystal word

--- src/scry/protocol/settings.cr
+++ src/scry/protocol/settings.cr
@@ -1,55 +1,19 @@
 require "json"
 
-# Add new configuration type on did_change_configuration_params.cr:6
 module Scry
-  # Configuration for vscode-crystal-ide
-  # https://github.com/kofno/crystal-ide
-  struct SettingsCrystalIDE
+  struct Settings
     JSON.mapping({
-      crystal_config: {type: CustomizationsCrystalIDE, key: "crystal-ide"},
+      crystal_config: {type: Customizations, key: /^crystal.*/, default: Customizations.from_json("{}")},
     })
   end
 
-  # :ditto:
-  struct CustomizationsCrystalIDE
+  struct Customizations
     JSON.mapping({
-      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems"},
-      backend:                String,
-      custom_command:         {type: String, key: "customCommand"},
-      custom_command_args:    {type: Array(String), key: "customCommandArgs"},
-      log_level:              {type: String, key: "logLevel"},
+      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
+      backend:                {type: String, default: "scry"},
+      custom_command:         {type: String, key: "customCommand", default: "scry"},
+      custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
+      log_level:              {type: String, key: "logLevel", default: "info"},
     })
   end
-
-  # Configuration for vscode-crystal-lang
-  # https://github.com/faustinoaq/vscode-crystal-lang
-  struct SettingsCrystalLang
-    JSON.mapping({
-      crystal_config: {type: CustomizationsCrystalLang, key: "crystal-lang"},
-    })
-  end
-
-  # :ditto:
-  struct CustomizationsCrystalLang
-    JSON.mapping({
-      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems"},
-      log_level:              {type: String, key: "logLevel"},
-    })
-  end
-
-  # Configuration for atom-crystal
-  # https://github.com/atom-crystal/scry
-  # struct SettingsAtomCrystal
-  #   JSON.mapping({
-  #     crystal_config: {type: CustomizationsAtomCrystal, key: "atom-crystal"},
-  #   })
-  # end
-
-  # :ditto:
-  # struct CustomizationsAtomCrystal
-  #   JSON.mapping({
-  #     max_number_of_problems: {type: Int32, key: "maxNumberOfProblems"},
-  #     log_level:              {type: String, key: "logLevel"},
-  #   })
-  # end
 end
