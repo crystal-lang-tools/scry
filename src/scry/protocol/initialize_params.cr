5de9d312338228740e1adf42c6cf120a4afa09e2
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-09 00:07:07 -0500

    Fixes InitializeParams to follow LSP specification
    * Save compatibility with scry code base
    * Adds rootUri as alternative non nilable
    * Keep rootPath as default and use rootUri when this isn't available
    * Makes process_id nilable

--- src/scry/protocol/initialize_params.cr
+++ src/scry/protocol/initialize_params.cr
@@ -3,8 +3,9 @@ require "json"
 module Scry
   struct InitializeParams
     JSON.mapping({
-      process_id:   {type: Int64 | Int32, key: "processId"},
-      root_uri:     {type: String, key: "rootPath"},
+      process_id:   {type: Int64 | Int32 | Nil, key: "processId"},
+      root_uri:     {type: String | Nil, key: "rootUri"},
+      root_path:    {type: String | Nil, key: "rootPath", nilable: true},
       capabilities: JSON::Any,
       trace:        String?,
     })
