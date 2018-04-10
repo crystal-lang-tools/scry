c2f9740fe0e9aeb844ff6d0e369cea8ea34598e2
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-09 22:42:26 -0500

    Fix formatting

--- src/scry/protocol/text_document_params.cr
+++ src/scry/protocol/text_document_params.cr
@@ -7,7 +7,7 @@ module Scry
     JSON.mapping({
       text_document: {type: (TextDocumentIdentifier | VersionedTextDocumentIdentifier), key: "textDocument"},
       text:          String?,
-      include_text:  {type: Bool, nilable: true, key: "includeText"}
+      include_text:  {type: Bool, nilable: true, key: "includeText"},
     }, true)
   end
 end
