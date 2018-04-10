712170922a7dddaa75d0f4b2e97ca9155b6e7b43
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-04-10 07:22:57 -0500

    Add optional includeText (#86)

--- src/scry/protocol/text_document_params.cr
+++ src/scry/protocol/text_document_params.cr
@@ -7,6 +7,7 @@ module Scry
     JSON.mapping({
       text_document: {type: (TextDocumentIdentifier | VersionedTextDocumentIdentifier), key: "textDocument"},
       text:          String?,
+      include_text:  {type: Bool, nilable: true, key: "includeText"},
     }, true)
   end
 end
