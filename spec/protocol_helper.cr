80eeaf055e44dc735897b75e034c9fee76331e1d
Filipe Correia <filipe.lp.correia@gmail.com>
2018-04-21 16:43:11 +0100

    Switches ProtocolHelper to Override the Context struct, fixes the spec

--- spec/protocol_helper.cr
+++ spec/protocol_helper.cr
@@ -1,12 +1,8 @@
 module Scry
-  module ProtocolHelper
-    def self.send_init(context, root_path)
+  struct Context
+    def test_send_init( root_path)
       message = %({ "jsonrpc": "2.0", "id": 1, "method": "initialize", "params": { "processId": 1, "rootPath": "#{root_path}", "capabilities": {} , "trace": "off"}})
-      response = context.dispatch(Message.new(message).parse)
-    end
-
-    def self.send_message(context, message)
-
+      dispatch(Message.new(message).parse)
     end
   end
 end
--- spec/scry/completion_provider_spec.cr
+++ spec/scry/completion_provider_spec.cr
@@ -8,13 +8,14 @@ module Scry
       result = __CONTEXT__.dispatch(procedure.parse)
 
       procedure = Message.new(%({"jsonrpc": "2.0", "id": 1, "method": "textDocument/completion", "params": {"textDocument":{"uri":"#{__FILE_PATH__}"},"position":{"line":0,"character":#{{{code}}.size}}}}))
-      result = __CONTEXT__.dispatch(procedure.parse)
-      # labels = results.map(&.label)
-      # labels.should eq({{with_labels}})
+      response = __CONTEXT__.dispatch(procedure.parse).as(Scry::ResponseMessage)
+      results = response.result.as(Array(CompletionItem))
+      labels = results.map(&.label)
+      labels.should eq({{with_labels}})
 
-      # results.each do |e|
-      #   e.kind.should eq(__KIND__)
-      # end
+      results.each do |e|
+        e.kind.should eq(__KIND__)
+      end
     end
   end
 
@@ -23,7 +24,7 @@ module Scry
       __KIND__ = CompletionItemKind::Module
       __CONTEXT__ = Context.new
       root_path = File.expand_path("spec/fixtures/completion/")
-      ProtocolHelper.send_init(__CONTEXT__, root_path)
+      __CONTEXT__.test_send_init(root_path)
 
       __FILE_PATH__ = File.join(root_path, "tree.cr")
 
