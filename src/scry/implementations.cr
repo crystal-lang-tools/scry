bb4d446423e0685330351ee06c9c170e9a6c7bee
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-23 14:37:05 -0500

    Add support for main file and remove spec and lib restriction

--- src/scry/implementations.cr
+++ src/scry/implementations.cr
@@ -36,10 +36,13 @@ module Scry
 
     def get_scope
       root_uri = @workspace.root_uri
-      if Dir.exists?("#{root_uri}/src")
+      main_file = "#{root_uri}/.scry.cr"
+      if File.exists?(main_file)
+        [main_file]
+      elsif Dir.exists?("#{root_uri}/src")
         Dir.glob("#{root_uri}/src/*.cr")
       else
-        Dir.glob("#{root_uri}/**/*.cr").reject(&.=~(/\/spec\/|\/lib\//))
+        Dir.glob("#{root_uri}/**/*.cr")
       end
     end
 
