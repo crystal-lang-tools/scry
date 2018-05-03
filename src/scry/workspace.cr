7fc0c98a7d8953f51f7b4b6e46b3b011bf03fe55
Filipe Correia <filipe.lp.correia@gmail.com>
2018-05-03 20:20:05 +0100

    Add completion to classes defined in own file

--- src/scry/workspace.cr
+++ src/scry/workspace.cr
@@ -27,7 +27,7 @@ module Scry
         @open_files[file.filename] = {file, Completion::MethodDB.new}
       else
         file_dependencies = @dependency_graph[file.filename].descendants.map &.value
-        method_db = Completion::MethodDB.generate(file_dependencies)
+        method_db = Completion::MethodDB.generate((file_dependencies + [file.filename]).uniq)
         @open_files[file.filename] = {file, method_db}
       end
     end
