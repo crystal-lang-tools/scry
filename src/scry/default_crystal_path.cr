44524e381373718e30182db268ff05d32a209d6b
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-04 17:41:30 -0500

    Use CrystalPath.default_path

--- src/scry/default_crystal_path.cr
+++ src/scry/default_crystal_path.cr
@@ -1,6 +1,6 @@
 module Scry
   def self.default_crystal_path
-    Crystal::DEFAULT_PATH.split(":").map do |path|
+    Crystal::CrystalPath.default_path.split(":").map do |path|
       if File.exists?(File.expand_path("prelude.cr", path))
         return path
       end
