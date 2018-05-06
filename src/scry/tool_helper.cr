e29312c184219e528b139e4008ebb9e320377c65
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-06 00:08:24 -0500

    Rename .scry.cr to .scry_main.cr

--- src/scry/tool_helper.cr
+++ src/scry/tool_helper.cr
@@ -1,6 +1,6 @@
 module Scry::ToolHelper
   def get_scope(root_uri)
-    main_file = File.join(root_uri, ".scry.cr")
+    main_file = File.join(root_uri, ".scry_main.cr")
     if File.exists?(main_file)
       [main_file]
     elsif Dir.exists?(File.join(root_uri, "src"))
