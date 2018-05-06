0ed48e745c92ddebe9d8d78c0a34b86e0eb2e8a6
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-05 20:56:47 -0500

    Use File.join

--- src/scry/tool_helper.cr
+++ src/scry/tool_helper.cr
@@ -1,12 +1,12 @@
 module Scry::ToolHelper
   def get_scope(root_uri)
-    main_file = "#{root_uri}/.scry.cr"
+    main_file = File.join(root_uri, ".scry.cr")
     if File.exists?(main_file)
       [main_file]
-    elsif Dir.exists?("#{root_uri}/src")
-      Dir.glob("#{root_uri}/src/*.cr")
+    elsif Dir.exists?(File.join(root_uri, "src"))
+      Dir.glob(File.join(root_uri, "src", "*.cr"))
     else
-      Dir.glob("#{root_uri}/**/*.cr")
+      Dir.glob(File.join(root_uri, "**", "*.cr"))
     end
   end
 end
