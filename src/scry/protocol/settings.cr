8742e0a45502f2c8ef1c1d8e03bc9731e28a0d9b
Faustino Aguilar <faustinoaq@gmail.com>
2018-06-16 19:04:55 -0500

    Disable Settings (will be replaced by .scry.yml)

--- src/scry/protocol/settings.cr
+++ src/scry/protocol/settings.cr
@@ -1,19 +1,20 @@
 require "json"
 
 module Scry
+  # Will be replaced by .scry.yml
   struct Settings
-    JSON.mapping({
-      crystal_config: {type: Customizations, key: /^crystal.*/, default: Customizations.from_json("{}")},
-    })
+    # JSON.mapping({
+    #   crystal_config: {type: Customizations, key: /^crystal.*/, default: Customizations.from_json("{}")},
+    # })
   end
 
-  struct Customizations
-    JSON.mapping({
-      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
-      backend:                {type: String, default: "scry"},
-      custom_command:         {type: String, key: "customCommand", default: "scry"},
-      custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
-      log_level:              {type: String, key: "logLevel", default: "info"},
-    })
-  end
+  # struct Customizations
+  #   JSON.mapping({
+  #     max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
+  #     backend:                {type: String, default: "scry"},
+  #     custom_command:         {type: String, key: "customCommand", default: "scry"},
+  #     custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
+  #     log_level:              {type: String, key: "logLevel", default: "info"},
+  #   })
+  # end
 end
