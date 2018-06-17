fc479b32afbe3f3eb6e4c4b89f46c2f61c39c0ff
Faustino Aguilar <faustinoaq@gmail.com>
2018-06-16 19:41:01 -0500

    Fix undefined constant

--- src/scry/protocol/settings.cr
+++ src/scry/protocol/settings.cr
@@ -9,13 +9,13 @@ module Scry
     })
   end
 
-  # struct Customizations
-  #   JSON.mapping({
-  #     max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
-  #     backend:                {type: String, default: "scry"},
-  #     custom_command:         {type: String, key: "customCommand", default: "scry"},
-  #     custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
-  #     log_level:              {type: String, key: "logLevel", default: "info"},
-  #   })
-  # end
+  struct Customizations
+    JSON.mapping({
+      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
+      backend:                {type: String, default: "scry"},
+      custom_command:         {type: String, key: "customCommand", default: "scry"},
+      custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
+      log_level:              {type: String, key: "logLevel", default: "info"},
+    })
+  end
 end
