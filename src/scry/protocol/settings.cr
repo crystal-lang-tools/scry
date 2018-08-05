23b1f00531a6be828b3a8abe06e35a4887414a41
Bryan Mulvihill <mulvihill@informz.com>
2018-08-05 10:13:39 -0400

    Use editor specific config settings

--- src/scry/protocol/settings.cr
+++ src/scry/protocol/settings.cr
@@ -1,19 +1,37 @@
 require "json"
 
 module Scry
-  struct Settings
-    JSON.mapping({
-      crystal_config: {type: Customizations, key: /^crystal.*/, default: Customizations.from_json("{}")},
-    })
-  end
+  alias Settings = SettingsCrystalIDE | SettingsCrystalLang
 
   struct Customizations
-    JSON.mapping({
-      max_number_of_problems: {type: Int32, key: "maxNumberOfProblems", default: 100},
-      backend:                {type: String, default: "scry"},
-      custom_command:         {type: String, key: "customCommand", default: "scry"},
-      custom_command_args:    {type: Array(String), key: "customCommandArgs", default: [] of String},
-      log_level:              {type: String, key: "logLevel", default: "info"},
-    })
+    include JSON::Serializable
+    @[JSON::Field(key: "maxNumberOfProblems")]
+    property max_number_of_problems : Int32 = 100
+    @[JSON::Field(key: "scry")]
+    property backend : String = "scry"
+    @[JSON::Field(key: "customCommand")]
+    property custom_command : String = "scry"
+    @[JSON::Field(key: "customCommandArgs")]
+    property custom_command_args : Array(String) = [] of String
+    @[JSON::Field(key: "logLevel")]
+    property log_level : String = "info"
+  end
+
+  # Configuration for vscode-crystal-ide
+  # https://github.com/kofno/crystal-ide
+  struct SettingsCrystalIDE
+    include JSON::Serializable
+
+    @[JSON::Field(key: "crystal-ide")]
+    property crystal_config : Customizations
+  end
+
+  # Configuration for vscode-crystal-lang
+  # https://github.com/faustinoaq/vscode-crystal-lang
+  struct SettingsCrystalLang
+    include JSON::Serializable
+
+    @[JSON::Field(key: "crystal-lang")]
+    property crystal_config : Customizations
   end
 end
