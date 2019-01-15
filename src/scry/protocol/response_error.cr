27d830c7451e51476666c9e456c21267645beecb
Matthew McGarvey <tnraptor14@hotmail.com>
2019-01-15 12:29:40 -0600

    override new instead of initialize for exception handling

--- src/scry/protocol/response_error.cr
+++ src/scry/protocol/response_error.cr
@@ -36,12 +36,12 @@ module Scry::Protocol
     def initialize(@message, @data = nil, @code = ErrorCodes::UnknownErrorCode)
     end
 
-    def initialize(ex : Exception)
-      initialize(ex.message || "Unknown error", ex.backtrace?)
+    def self.new(ex : Exception)
+      new(ex.message || "Unknown error", ex.backtrace?)
     end
 
-    def initialize(ex : ProtocolException)
-      initialize(ex.message || "An error occurred", ex.backtrace?, ex.code)
+    def self.new(ex : ProtocolException)
+      new(ex.message || "An error occurred", ex.backtrace?, ex.code)
     end
   end
 end
