0b08e0cd830fb9d88d8ff71fd8d69d03ab0e7856
Davis Namsons <davisnamsons@gmail.com>
2019-09-29 12:36:11 +0300

    Add Settings back

---
+++ src/scry/settings.cr
@@ -0,0 +1,35 @@
+module Scry
+  alias Settings = SettingsCrystalIDE | SettingsCrystalLang
+
+  struct Customizations
+    include JSON::Serializable
+    @[JSON::Field(key: "maxNumberOfProblems")]
+    property max_number_of_problems : Int32 = 100
+    @[JSON::Field(key: "scry")]
+    property backend : String = "scry"
+    @[JSON::Field(key: "customCommand")]
+    property custom_command : String = "scry"
+    @[JSON::Field(key: "customCommandArgs")]
+    property custom_command_args : Array(String) = [] of String
+    @[JSON::Field(key: "logLevel")]
+    property log_level : String = "info"
+  end
+
+  # Configuration for vscode-crystal-ide
+  # https://github.com/kofno/crystal-ide
+  struct SettingsCrystalIDE
+    include JSON::Serializable
+
+    @[JSON::Field(key: "crystal-ide")]
+    property crystal_config : Customizations
+  end
+
+  # Configuration for vscode-crystal-lang
+  # https://github.com/faustinoaq/vscode-crystal-lang
+  struct SettingsCrystalLang
+    include JSON::Serializable
+
+    @[JSON::Field(key: "crystal-lang")]
+    property crystal_config : Customizations
+  end
+end
